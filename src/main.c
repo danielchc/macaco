f03c777a5d33d00fa5b040ad02ebf9d109e60b8e
Dani <danielchenelcea@gmail.com>
2021-10-06 16:54:14 +0200

    Leer arquivo

--- src/main.c
+++ src/main.c
@@ -5,12 +5,18 @@
 int main(int argc, char* argv[]){
     
     ts_s k;
-
     init_ts(&k);
-
-    printf("%p\n",k);
-
+    //printf("%p\n",k);
     delete_ts(&k);
-
+	
+	FILE *file = fopen("wilcoxon.py", "r");
+	char current='\0';
+	do{
+		current = (char)fgetc(file);
+		if(current=='\n') continue;
+		printf("%c\n",current);
+	}while(current != EOF);
+	
+	
     return 0;
 }
