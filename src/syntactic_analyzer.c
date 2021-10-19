906955846304abf45c687d6777fcb9d7b85aef5e
Dani <danielchenelcea@gmail.com>
2021-10-19 14:57:00 +0200

    comments s_a.c

--- src/syntactic_analyzer.c
+++ src/syntactic_analyzer.c
@@ -1,12 +1,17 @@
 #include "syntactic_analyzer.h"
 
 void init_analyze(){
+	//Reservo o tamaño do lexema
 	lexcomp_t* current_lex=malloc(sizeof(lexcomp_t));
-	char output[200];
-	int line=1;
+	//Emprego esta variable para imprimir copiar o valor do lexema ou EOF, e o NEWLINE
+	char output[KEYWORDSIZE];
+	int line;
 	do{
+		//Se é un compoñente léxico 
 		if (next_lexcomp(current_lex)!=AT_NOLEX){
+			//Obteño o número de liña que se está procesando
 			line=get_current_line();
+			//Se é unha salto de linea ou un fin de ficheiro poñoo por texto, senon cargo a palabra
 			if(current_lex->value==_NEWLINE){
 				strcpy(output,"<NEWLINE>");
 			}else if(current_lex->value==_EOF){
@@ -14,7 +19,7 @@ void init_analyze(){
 			}else{
 				strcpy(output,current_lex->keyword);
 			}
-			if(current_lex->value==0)continue;
+			//Imprimo por pantalla o compoñente lexico
 			printf("LINEA %d ->\tTIPO [%d]\t%s\n",line,current_lex->value,output);
 		};
 		
